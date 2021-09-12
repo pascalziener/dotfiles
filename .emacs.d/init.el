d593eef25c60ea3e71c230ff4ebf9c5100173603
Pascal Ziener <pzi.git@outlook.com>
2021-09-12 22:33:21 +0200

    Initial Emacs config

---
+++ .emacs.d/init.el
@@ -0,0 +1,15 @@
+;; PZI emacs config
+;; Build with the help of Emacs From Scratch series by System Crafters
+;; https://www.youtube.com/playlist?list=PLEoMzSkcN8oPH1au7H6B7bBJ4ZO7BXjSZ
+;;
+;; ...and several other sources
+
+;; Disable startup message
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode -1)   ; Disable visible scrollbar
+(tool-bar-mode -1)     ; Disable the toolbar
+(tooltip-mode -1)      ; Disable tooltips
+(set-fringe-mode 10)   ; Give some breathing room
+;; Keep the menu enabled for now
+;;(menu-bar-mode -1)     ; Disable the menu bar
