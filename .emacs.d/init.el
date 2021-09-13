dc0434e4a1b7b4f072d11805e57623369c4896c2
Pascal Ziener <pzi.git@outlook.com>
2021-09-13 23:08:49 +0200

    add: completion framework ivy add with basic configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -24,6 +24,25 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Use Ivy for completions
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
 
 ;; ** User Interface
 ;; Clean up Emacs' user interface, make it more minimal.
@@ -41,6 +60,9 @@
 ;; Set up the visible bell
 (setq visible-bell t)
 
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
 ;; ** Font
 ;; *** Set the font
 (set-face-attribute 'default nil :font "Fira Code Retina" :height 110)
@@ -49,3 +71,15 @@
 ;; *** Set a theme
 (load-theme 'tango-dark)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ivy use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
